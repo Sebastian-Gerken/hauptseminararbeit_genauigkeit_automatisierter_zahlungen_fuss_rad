26e7d448d6eac1e49c57102563363346883a72dc
Sebastian-Gerken <sebastian-gerke@hotmail.com>
2023-04-09 16:52:38 +0200

    cleanup

--- Plot.R
+++ Plot.R
@@ -2,28 +2,24 @@ library(tidyverse)
 library(reshape2)
 library(data.table)
 
-df_gt <- read_csv("data/gt_data/TUDCam01_FR20_2022_09_20_08-00-00.csv")
-df_eval <- read_csv("data/otv_data/TUDCam01_FR20_2022-09-20_08-00-00_events.csv")
+df_gt <- read_csv(file.choose())
+df_eval <- read_csv(file.choose())
 
 
-df_count <- as.data.table(empty)
-df_gt$Class
 ggplot(data = df_gt[df_gt$Class == "Person",  ]) +
-  geom_point(aes(X,Y)) +
-  scale_x_continuous(limits = c(0, 799), expand = c(0, 0)) +
-  scale_y_continuous(limits = c(0, 599), expand = c(0, 0))
+  #geom_point(aes(X,Y)) +
+  #scale_x_continuous(limits = c(0, 799), expand = c(0, 0)) +
+  #scale_y_continuous(limits = c(0, 599), expand = c(0, 0)) + 
+  geom_raster(aes(X,Y), interpolate = T)
 
-
-bins_count = 10
-df_eval[df_eval$Class == "",]
 
 ggplot(df_eval[ df_eval$Class == "person",] , aes(X, Y)) +
-  geom_density2d() +
+  geom_density2d_filled() +
   scale_x_continuous(limits = c(0, 799), expand = c(0, 0)) +
   scale_y_continuous(limits = c(0, 599), expand = c(0, 0)) +
   theme_classic()
 
-plt_eval <- ggplot(df_eval, aes(X, Y)) +
+ggplot(df_eval, aes(X, Y)) +
   stat_bin2d(bins = bins_count)
 
 diff <- plt_eval$data$c - plt_gt$data
