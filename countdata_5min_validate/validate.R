4ed9f158762a022cff0001df3b378a233fffb77b
Sebastian-Gerken <sebastian-gerken@hotmail.com>
2023-05-23 17:55:13 +0200

    data cleanup

--- countdata_5min_validate/validate.R
+++
@@ -1,8 +0,0 @@
-setwd("C:/Users/sebas/OneDrive/8. Semester/Hauptseminararbeit/Auswertung/countdata_5min_validate")
-library(readr)
-df1 <- read_delim("1count_TUDCam01_2022-09-20_08-00-00/Nord-00_05_00-08_00_00-08_15_00.csv", 
-                  delim = ";", escape_double = FALSE, trim_ws = TRUE)
-
-df2 <- read_delim("count_TUDCam01_2022-09-20_08-00-00/Nord-00_05_00-08_00_00-08_15_00.csv", 
-                                              delim = ";", escape_double = FALSE, trim_ws = TRUE)
-df1 == df2
